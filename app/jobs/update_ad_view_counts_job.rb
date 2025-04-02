3ee9f0f899e9dbad8e52efb60f498b834bd13dcb
Mauricio Barros <50715970+maurowebdev@users.noreply.github.com>
2025-04-02 12:43:09 -0500

    Prevent reducing view_count

--- app/jobs/update_ad_view_counts_job.rb
+++ app/jobs/update_ad_view_counts_job.rb
@@ -5,7 +5,7 @@ class UpdateAdViewCountsJob < ApplicationJob
     Ad.published.find_each do |ad|
       redis = Redis.new
       view_count = redis.get("ad_#{ad.id}_view_count").to_i
-      next if view_count == ad.view_count || view_count.zero?
+      next if view_count <= ad.view_count || view_count.zero?
 
       ad.update(view_count: view_count)
     end
